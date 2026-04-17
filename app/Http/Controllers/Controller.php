7c127040282d41edf07fb59a839cbc902e7ad515
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-04-17 16:23:41 +0700

    Add method to retrieve authenticated user's username in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
     use \App\Support\Concerns\ApiResponse;
+
+    protected function getUserName(): string
+    {
+        return Auth::user()->username;
+    }
 }
