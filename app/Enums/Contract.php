bc760ace197af45d2fb10c5c7d1693f9d17c06f6
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-04-17 10:10:36 +0700

    Add enums for Contract and Position with associated values

--- app/Enums/Contract.php
+++ app/Enums/Contract.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Enums;
+
+use App\Enums\Concerns\HasValues;
+
+enum Contract: string
+{
+    use HasValues;
+
+    case FULL_TIME  = 'Full-time';
+    case PART_TIME  = 'Part-time';
+    case INTERN = 'Intern';
+    case COLLABORATOR = 'Collaborator';
+    case SEASONAL = 'Seasonal';
+}
--- app/Enums/Position.php
+++ app/Enums/Position.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace App\Enums;
+
+use App\Enums\Concerns\HasValues;
+
+enum Position: string
+{
+    use HasValues;
+
+    case SINGER = 'Singer';
+    case MC = 'MC';
+    case RAPPER = 'Rapper';
+    case DANCER = 'Dancer';
+    case CHOREOGRAPHER = 'Chorographer'; //Biên đạo múa/ nhảy
+    case LOGISTICS_STAFF = 'Logistics Staff'; //Nhân viên hậu cần
+    case SOUND_TECHNICIAN = 'Sound Technician';
+    case LIGHTING_TECHNICIAN = 'Lighting Technician';
+    case WAREHOUSE_STAFF = 'Warehouse Staff';
+    case OFFICE_STAFF = 'Office Staff';
+}
