450e7c56a703778284e43f48cbe03088c11fa8aa
hieu92264 <nguyenthehieu20182021@gmail.com>
2026-04-22 16:14:03 +0700

    Add test comment for CI/CD in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ abstract class Controller
 {
     use \App\Support\Concerns\ApiResponse;
 
+    //test ci/cd
     protected function getUserName(): string
     {
         return Auth::user()->username;
